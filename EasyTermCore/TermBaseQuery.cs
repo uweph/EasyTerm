238e90249158b5d8f34fa9fbd9323ded04319727
Uwe <uweph@gmx.de>
2015-10-25 14:22:59 +0100

    Missing files

---
+++ EasyTermCore/TermBaseQuery.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTermCore
+{
+    // --------------------------------------------------------------------------------
+    /// <summary>
+    /// Access to all term base queries
+    /// </summary>
+    // --------------------------------------------------------------------------------
+    public class TermBaseQuery : IDisposable
+    {
+        private TermBaseSet _TermbaseSet;
+        private TermBaseQueryWorker _Worker;
+
+        internal TermBaseQuery(TermBaseSet termbaseSet)
+        {
+            _TermbaseSet = termbaseSet;
+            _Worker = new TermBaseQueryWorker(this, _TermbaseSet);
+        }
+
+
+
+#region Public query functions
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lang1"></param>
+        /// <param name="lang2"></param>
+        /// <returns></returns>
+        /// <created>UPh,24.10.2015</created>
+        /// <changed>UPh,24.10.2015</changed>
+        // ********************************************************************************
+        public void SetLanguagePair(CultureInfo lang1, CultureInfo lang2)
+        {
+            
+        
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        public void RequestTerminology(string word)
+        {
+            if (!_Worker.IsStarted)
+                _Worker.Start();
+
+            _Worker.PutRequest(TermBaseRequest.MakeTerminologyRequest(word));
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        public void RequestTermList()
+        {
+            if (!_Worker.IsStarted)
+                _Worker.Start();
+
+            _Worker.PutRequest(TermBaseRequest.MakeTermListRequest());
+        }
+
+
+        // TODO needs EventArgs
+        public delegate void TerminologyResultHandler(object sender, EventArgs e);
+        public event TerminologyResultHandler TerminologyResult;
+
+        public delegate void TermListResultHandler(object sender, TermListResultArgs e);
+        public event TermListResultHandler TermListResult;
+
+
+        // From worker
+        internal void FireTermListResult(int requestid, TermListItems items)
+        {
+            if (TermListResult == null)
+                return;
+
+            TermListResultArgs args = new TermListResultArgs();
+            args.RequestID = requestid;
+            args.Items = items;
+
+            TermListResult(this, args);
+        }
+        
+
+
+        
+        
+#endregion
+
+
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,24.10.2015</created>
+        /// <changed>UPh,24.10.2015</changed>
+        // ********************************************************************************
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        bool _bDisposed = false;
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="disposing"></param>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_bDisposed)
+                return;
+
+            if (disposing)
+            {
+                _Worker.Stop();
+
+                
+            }
+
+            _bDisposed = true;
+
+        }
+    }
+
+    // --------------------------------------------------------------------------------
+    /// <summary>
+    /// 
+    /// </summary>
+    // --------------------------------------------------------------------------------
+    public class TermListItem
+    {
+        public string Term {get; internal set;}
+        public int TermID {get; set;}
+    }
+
+    // --------------------------------------------------------------------------------
+    /// <summary>
+    /// 
+    /// </summary>
+    // --------------------------------------------------------------------------------
+    public class TermListItems : List<TermListItem>
+    {
+
+    }
+
+    // --------------------------------------------------------------------------------
+    /// <summary>
+    /// 
+    /// </summary>
+    // --------------------------------------------------------------------------------
+    public class TermListResultArgs : EventArgs
+    {
+        public int RequestID {get; set;}
+        public TermListItems Items {get; internal set;}
+    }
+
+}
---
+++ EasyTermCore/TermBaseQueryWorker.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyTermCore
+{
+    // --------------------------------------------------------------------------------
+    /// <summary>
+    /// 
+    /// </summary>
+    // --------------------------------------------------------------------------------
+    internal class TermBaseQueryWorker
+    {
+        private TermBaseSet _TermbaseSet;
+        private TermBaseQuery _TermbaseQuery;
+        private volatile bool _shouldStop = false;
+        private Thread _Thread;
+        private AutoResetEvent _DataAvailableEvent;
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="termbaseSet"></param>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        internal TermBaseQueryWorker(TermBaseQuery termbaseQuery, TermBaseSet termbaseSet)
+        {
+            _TermbaseQuery = termbaseQuery;
+            _TermbaseSet = termbaseSet;
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        internal bool IsStarted
+        {
+            get
+            {
+                return _Thread != null;
+            }
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        internal void Start()
+        {
+            try
+            {
+                Stop();
+
+                _shouldStop = false;
+                _DataAvailableEvent = new AutoResetEvent(false);
+                _Thread = new Thread(WorkerThread);
+                _Thread.IsBackground = true;
+                _Thread.Start();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Cannot start thread.", ex);
+            }
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        internal void Stop()
+        {
+            if (_Thread == null)
+                return;
+
+            try
+            {
+                RequestStop();
+                _Thread.Join(5000);
+
+                _Thread = null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Cannot stop thread.", ex);
+            }
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        internal void RequestStop()
+        {
+            _shouldStop = true;
+            _DataAvailableEvent.Set();
+        }
+
+        volatile bool _Paused = false;
+        internal void PauseRequests()
+        {
+            lock (_NewRequests)
+            {
+                _NewRequests.Clear();
+                _Paused = true;
+            }
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        internal void ContinueQueries()
+        {
+            _Paused = false;
+        }
+
+
+
+        private List<TermBaseRequest> _NewRequests = new List<TermBaseRequest>();
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        internal void PutRequest(TermBaseRequest request)
+        {
+            if (_Paused)
+                return;
+
+            lock (_NewRequests)
+            {
+                _NewRequests.Clear();      // Clear all outstanding requests
+                _NewRequests.Add(request); 
+                _DataAvailableEvent.Set();
+            }
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        private TermBaseRequest GetRequest()
+        {
+            lock (_NewRequests)
+            {
+                if (_NewRequests.Count == 0)
+                    return null;
+
+                TermBaseRequest request = _NewRequests[0];
+                _NewRequests.RemoveAt(0);
+                return request;
+            }
+        }   
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        private void WorkerThread()
+        {
+            while (!_shouldStop)
+            {
+                _DataAvailableEvent.WaitOne(1000);
+                if (_shouldStop)
+                    return;
+
+                TermBaseRequest request = GetRequest();
+                if (request == null)
+                    continue;
+
+
+                if (request.Type == RequestType.TermList)
+                {
+                    HandleTermListRequest(request);
+                }
+                else if (request.Type == RequestType.TermList)
+                {
+                    HandleTerminologyRequest(request);
+                }
+
+                // TODO Bei _PauseRequest die aktuelle Suche abbrechen
+            }
+
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        private void HandleTermListRequest(TermBaseRequest request)
+        {
+            TermListItems items = new TermListItems();
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                if (_Paused || _shouldStop)
+                    return;
+
+                TermListItem item = new TermListItem();
+                item.Term = string.Format("Term {0}", i);
+
+                items.Add(item);
+            
+            }  
+
+            _TermbaseQuery.FireTermListResult(request.ID, items);
+            
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        private void HandleTerminologyRequest(TermBaseRequest request)
+        {
+        }
+
+    }
+
+    enum RequestType
+    {
+        TermList,
+        Terminology
+    };
+
+
+    // --------------------------------------------------------------------------------
+    /// <summary>
+    /// 
+    /// </summary>
+    // --------------------------------------------------------------------------------
+    internal class TermBaseRequest
+    {   
+        internal RequestType Type{get; private set;}
+        internal string Term {get; private set;}
+        internal int ID {get; set;}
+
+        static int _NewRequestID = 100;
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        static internal TermBaseRequest MakeTermListRequest()
+        {
+            TermBaseRequest request = new TermBaseRequest();
+            Interlocked.Increment(ref _NewRequestID);
+            request.ID = _NewRequestID;
+            request.Type = RequestType.TermList;
+            return request;
+        }
+
+        // ********************************************************************************
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="term"></param>
+        /// <returns></returns>
+        /// <created>UPh,25.10.2015</created>
+        /// <changed>UPh,25.10.2015</changed>
+        // ********************************************************************************
+        static internal TermBaseRequest MakeTerminologyRequest(string term)
+        {
+            TermBaseRequest request = new TermBaseRequest();
+            request.ID = 0;
+            request.Type = RequestType.Terminology;
+            request.Term = term;
+            return request;
+        }
+    }
+}
